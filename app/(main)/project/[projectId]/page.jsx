0bd8bf7aaf486dd807eb4a8cb0f5f936dc74c93b
ayushpanwar <ayushpanwar132003@gmail.com>
2025-06-17 21:02:22 +0530

    updated working

--- app/(main)/project/[projectId]/page.jsx
+++ app/(main)/project/[projectId]/page.jsx
@@ -1,14 +1,16 @@
 import { getProject } from "@/actions/projects";
 import { notFound } from "next/navigation";
-import { currentOrganization } from "@clerk/nextjs/server";
-
-// import SprintCreationForm from "../_components/create-sprint";
-// import SprintBoard from "../_components/sprint-board";
+import { auth } from "@clerk/nextjs/server";
+import SprintCreationForm from "../_components/create-sprint";
+import SprintBoard from "../_components/sprint-board";
 
 export default async function ProjectPage({ params }) {
   const { projectId } = params;
-  const org = await currentOrganization();
-  const orgId = org?.id;
+  console.log('project id:',projectId)
+const { sessionClaims } = await auth();
+const orgId = sessionClaims?.o?.id;
+
+  console.log('orgIdddddd:',orgId)
 
   if (!orgId) {
     notFound(); // Could also throw error
@@ -20,27 +22,27 @@ export default async function ProjectPage({ params }) {
     notFound();
   }
 
-  // return (
-  //   <div className="container mx-auto">
-  //     <SprintCreationForm
-  //       projectTitle={project.name}
-  //       projectId={projectId}
-  //       projectKey={project.key}
-  //       sprintKey={project.sprints?.length + 1}
-  //     />
-
-  //     {project.sprints.length > 0 ? (
-  //       <SprintBoard
-  //         sprints={project.sprints}
-  //         projectId={projectId}
-  //         orgId={project.organizationId}
-  //       />
-  //     ) : (
-  //       <div>Create a Sprint from button above</div>
-  //     )}
-  //   </div>
-  // );
   return (
-    <div>hjhjfhshrud</div>
+    <div>
+      Project Page
+    </div>
+    // <div className="container mx-auto">
+    //   <SprintCreationForm
+    //     projectTitle={project.name}
+    //     projectId={projectId}
+    //     projectKey={project.key}
+    //     sprintKey={project.sprints?.length + 1}
+    //   />
+
+    //   {project.sprints.length > 0 ? (
+    //     <SprintBoard
+    //       sprints={project.sprints}
+    //       projectId={projectId}
+    //       orgId={project.organizationId}
+    //     />
+    //   ) : (
+    //     <div>Create a Sprint from button above</div>
+    //   )}
+    // </div>
   );
 }
